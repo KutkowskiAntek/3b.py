b7d2034d8914246a6a7d05c32b59d8dfcb59ed72
Antek Kutkowski <113889782+ant2oo7@users.noreply.github.com>
2022-10-25 20:55:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# zad 1
+# for i in range(1,32):
+#     print(i)
+
+# zad 2
+# n = int(input())
+# for i in range(1,n + 1,2):
+#     print(i, end=" ")
+
+# zad 3
+# for i in range(1000,10000):
+#     if i%379==0:
+#         print(i, end=" ")
+#         pass
+
+# zad 4
+# for i in range(100,1000):
+#     if i%5==0 or i%6==0 or i%11==0:
+#         print(i)
+#         pass
+
+# zad 5
+# suma = 0
+# n = int(input())
+# for i in range(n):
+#     a = int(input())
+#     suma = suma + a
+# print(suma)
+
+# zad 6
+# suma = 0
+# k = int(input())
+# for i in range(2,k*2+2,2):
+#     suma = suma + i
+# print(suma)
